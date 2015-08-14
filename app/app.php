8a11cd77b94be765579a6b02ea55211606681b0c
calascionec <chris.calascione@gmail.com>
2015-08-14 13:10:40 -0700

    Update twig results file

--- app/app.php
+++ app/app.php
@@ -5,6 +5,7 @@
 
 
     $app = new Silex\Application();
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__."/../views"));
 
     $app->get("/", function() use ($app) {
